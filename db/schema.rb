9bd76f7067b1d04abf3cea109a5c148dcd0764d6
dongyeoplee <afootjioo@nate.com>
2014-12-14 01:17:49 +0900

    modeling users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209140451) do
+ActiveRecord::Schema.define(version: 20141213155149) do
 
   create_table "microposts", force: true do |t|
     t.text     "content"
@@ -23,8 +23,11 @@ ActiveRecord::Schema.define(version: 20141209140451) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
